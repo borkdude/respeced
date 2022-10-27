1b7a09c3d6819277a83f1f6d19b634a396004b41
ikappaki <34983288+ikappaki@users.noreply.github.com>
2022-10-27 20:16:45 +0100

    Fix i#4: ssue with different `caught?` exception msg in Clojure 1.11 (#5)
    * Fix issue with different `caught?` exception msg in Clojure 1.11
    
    Co-authored-by: ikappaki <ikappaki@users.noreply.github.com>

---
+++ bb/tasks.clj
@@ -0,0 +1,26 @@
+(ns tasks
+  (:require [babashka.deps :as deps]
+            [babashka.fs :as fs]
+            [babashka.process :as p]))
+
+(defn clj-test
+  "Runs the clojure tests using the deps DEPS-CLJ-VER-ALIAS (presumambly
+  an alias that sets the clojure version to use as dep)."
+  [deps-clj-ver-alias]
+  (println "=== Running clj-tests" deps-clj-ver-alias)
+  (let [alias (format "-A:test%s:clj-tests" deps-clj-ver-alias)]
+    (fs/delete-tree "out")
+    (-> (deps/clojure [alias])
+        p/check)))
+
+(defn cljs-test
+  "Runs the clojure tests using the deps DEPS-CLJ-VER-ALIAS (presumambly
+  an alias that sets the clojurescript version to use as dep)."
+  [deps-clj-ver-alias]
+  (let [alias (format "-A:test%s:compile-node-tests" deps-clj-ver-alias)]
+    (println "\n=== Compiling node tests" deps-clj-ver-alias)
+    (-> (deps/clojure [alias])
+        p/check)
+    (println "\n=== Running node tests")
+    (p/shell "node out/main.js")))
+
